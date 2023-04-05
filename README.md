eb087de7708b58bbd45c3831c5de92e7961af88a
Taemour Zaidi <tzaidi24@wooster.edu>
2023-04-04 22:19:21 -0400

    intermediate code 2

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## By Taemour Zaidi
 
 This software will take in an image and output a .txt file containing ASCII Art representation of the image. Depending on how much development time I have left, it may be ported to mobile devices so it is easier for people to use.
+
+### Guides followed:
+https://stackoverflow.com/questions/55508615/how-to-pixelate-image-using-opencv-in-python
+https://www.geeksforgeeks.org/converting-image-ascii-image-python/
--- main.py
+++ main.py
@@ -1,41 +1,151 @@
-import cv2
+from PIL import Image
+import numpy as np
+import argparse
 
-# Input image
-print("Enter image: ", end = '')
-image = input()
-default = 'homapage.jpeg'
-image = cv2.imread("./" + default)
+# gray scale level values from:
+# http://paulbourke.net/dataformats/asciiart/
+global charscale
+charscale = "@%#*+=-:. "
 
-# Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def averageBrightness(image):
+    """
+    Given PIL Image, return average value of grayscale value
+    """
+    # get image as numpy array
+    im = np.array(image)
+ 
+    # get shape
+    w,h = im.shape
+
+    # get average
+    return np.average(im.reshape(w*h))
+
+def asciiOutput(inFile, outSize, letterScale):
+
+    # open image and convert to grayscale
+    image = Image.open(inFile).convert('L')
+ 
+    # input image dimensions
+    W, H = image.size[0], image.size[1]
+
+    # compute tile dimensions and no. rows
+    w = W/outSize
+    h = w/letterScale
+    rows = int(H/h)
+
+    """ Debug stuff
+    image.show()
+    print("INPUT IMAGE:\twidth: %d \theight: %d" % (W, H))
+    print("OUTPUT ASCII:\tcols: %d \trows: %d" % (outSize, rows))
+    print("TIME DIMS:\t%d x %d" % (w, h))
+    print("__________________")
+    """
+
+    # ascii image is a list of character strings
+    aimg = []
+    # generate list of dimensions
+    for i in range(rows):
+        y1 = int(i*h)
+        y2 = int((i+1)*h)
+ 
+        # correct last tile
+        if i == rows-1:
+            y2 = H
+ 
+        # append an empty string
+        aimg.append("")
+ 
+        for j in range(outSize):
+ 
+            # crop image to tile
+            x1 = int(j*w)
+            x2 = int((j+1)*w)
+ 
+            # correct last tile
+            if j == outSize-1:
+                x2 = W
+ 
+            # crop image to extract tile
+            img = image.crop((x1, y1, x2, y2))
+ 
+            # get average luminance
+            avg = int(averageBrightness(img))
+ 
+            # look up ascii char
+            gsval = charscale[int((avg*9)/255)]
+ 
+            # append ascii char to string
+            aimg[i] += gsval
+     
+    # return txt image
+    return aimg
+
+"""
+print(np.average([1,2,3,4,5]))
+image = 'homapage.jpeg'
+arr = asciiOutput(image, 100, 0.43)
+for row in arr:
+    print(row)
+"""
+def main():
+    parser = argparse.ArgumentParser(
+    description="Converts an image to an output text file containing an ASCII art approximation of the input.",
+    epilog="usage: python3 main.py -i [input] -o [ouput] -t")
+    parser.add_argument('-i', '--input', dest = 'inputImg', help = "path to the input file", required = True)
+    parser.add_argument('-o', '--output', dest = 'outFile', help = "output file path for the ASCII art [default = out.txt]", required = False)
+    parser.add_argument('-s', '--size', dest = 'size', help = "number of columns for output ASCII art [default = 50]", required = False)
+    parser.add_argument('-r', '--ratio', dest = 'ratio', help = "ratio of height/width of text font [default = 0.45]", required = False)
+    parser.add_argument('-t', '--terminal', dest = 'terminal', help = "print ASCII art to terminal", action = 'store_true')
+
+    args = parser.parse_args()
+
+    inputImg = args.inputImg
+
+    outFile = 'out.txt'
+    if args.outFile:
+        outFile = args.outFile
+
+    size = 50
+    if args.size:
+        size = int(args.size)
+
+    ratio = 0.45
+    if args.ratio:
+        ratio = float(args.ratio)
+
+    print('START...')
+    asciiImg = asciiOutput(inputImg, size, ratio)
+
+    out = open(outFile, 'w')
+    if (args.terminal):
+        for row in asciiImg:
+            out.write(row + '\n')
+            print(row)
+    else:
+        for row in asciiImg:
+            out.write(row + '\n')
+    
+    out.close()
+    print('...DONE')
+    
+if __name__ == '__main__':
+    main()
+
+"""
+TBA:
+- increase contrast
+- input video
+- play ASCII video
+- port?
+"""
+    
+    
 
 
-# Increase contrast
-contrast_gray = cv2.convertScaleAbs(gray, 200, 1.2)
-
-# Pixelate image. Hacky solution, work on it more
-
-# Set size of pixels
-height = gray.shape[0]
-width = gray.shape[1]
-# print(height, width)
-
-w, h = (int(height/3), int(width/3))
-
-# Resize input to "pixelated" size
-temp = cv2.resize(contrast_gray, (w, h), interpolation=cv2.INTER_LINEAR)
-output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
 
 
-# Display images
-cv2.imshow('Original image',image)
-cv2.waitKey(0)
-cv2.imshow('Grayscale', gray)
-cv2.waitKey(0)
-cv2.imshow('Contrast Up', contrast_gray)
-cv2.waitKey(0)
-cv2.imshow('Pixelated', output)
-cv2.waitKey(0)
 
 
-cv2.destroyAllWindows()
+
+
+
